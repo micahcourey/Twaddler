74fa66aa0fd3c10ad52cbb0ce782edd3d7e6407b
micahcourey <micahjcourey@gmail.com>
2015-11-28 22:11:46 -0800

    Restructure files moving the angular.js app into the public folder so it can be served by node.js

--- app.js
+++ app.js
@@ -1,48 +1,75 @@
-var twaddler = angular.module('twaddler', ['ngRoute']);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+//initialize mongoose schemas
+require('./models/models');
+var index = require('./routes/index');
+var api = require('./routes/api');
+var authenticate = require('./routes/authenticate')(passport);
+var mongoose = require('mongoose');                         //add for Mongo support
+mongoose.connect('mongodb://localhost/test-chirp');              //connect to Mongo
+var app = express();
 
-twaddler.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'templates/main.html',
-      controller: 'mainCtrl'
-    })
-    .when('/login', {
-      templateUrl: 'templates/login.html',
-      controller: 'authController'
-    })
-    .when('/signup', {
-      templateUrl: 'templates/signup.html',
-      controller: 'authController'
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(session({
+  secret: 'keyboard cat'
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', index);
+app.use('/auth', authenticate);
+app.use('/api', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//// Initialize Passport
+var initPassport = require('./passport-init');
+initPassport(passport);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
     });
 });
 
-twaddler.directive('navbar', function() {
-  return {
-    templateUrl: "templates/navbar.html"
-  }
-})
 
-twaddler.controller('mainCtrl', function($scope) {
-
-  $scope.twaddles = [];
-  $scope.newTwaddle = {creator: '', text: '', created: ''};
-
-  $scope.twaddle = function() {
-    $scope.newTwaddle.created = Date.now();
-    $scope.twaddles.push($scope.newTwaddle);
-    $scope.newTwaddle = {creator: '', text: '', created: ''};
-  };
-});
-
-twaddler.controller('authCtrl', function($scope) {
-  $scope.user = {username: '', password: ''};
-  $scope.error_message = '';
-
-  $scope.login = function() {
-    $scope.error_message = 'login request for ' + $scope.user.username;
-  };
-
-  $scope.signup = function() {
-    $scope.error_message = 'signup request for ' + $scope.user.username;
-  };
-});
+module.exports = app;
--- controllers/headersCtrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('twaddler')
-.controller('headersCtrl', function headersCtrl($scope, dataService) {
-
-});
